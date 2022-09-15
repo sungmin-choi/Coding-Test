2c8ad7ddfb37cc79fd614beac4bce9da5299aed7
sungmin choi <sungmin@thetrive.com>
2022-09-16 00:45:43 +0900

    1레벨 1일차

---
+++ sungmin-choi/레벨1/x만큼_간격이_있는_n개의_숫자.js
@@ -0,0 +1,11 @@
+function solution(x, n) {
+  const answer = [];
+  let curr = x;
+  for (let i = 0; i < n; i++) {
+    if (i > 0) {
+      curr += x;
+    }
+    answer.push(curr);
+  }
+  return answer;
+}
