9ad2584b8b99d61b2e3531c08e788d4df9905dfa
sungmin choi <namja306@naver.com>
2022-09-24 23:56:26 +0900

    짝지어_제거하기 수정

--- sungmin-choi/레벨2/짝지어_제거하기.js
+++ sungmin-choi/레벨2/짝지어_제거하기.js
@@ -1,18 +1,17 @@
 function solution(s) {
-  let answer = [0];
+  let answer = [];
   for (const i of s) {
-    const top = answer.length - 1;
-    if (answer.length >= 2) {
-      if (answer[top] === i) {
+    if (answer.length === 0) {
+      answer.push(i);
+    } else {
+      if (answer[answer.length - 1] === i) {
         answer.pop();
       } else {
         answer.push(i);
       }
-    } else {
-      answer.push(i);
     }
   }
-  return answer.length > 2 ? 0 : 1;
+  return answer.length > 0 ? 0 : 1;
 }
 
 console.log(solution("abccaeeaba"));
