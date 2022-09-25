c9e29ae0406d79fda835e4a7228d60f6b758d72b
sungmin choi <namja306@naver.com>
2022-09-25 21:00:21 +0900

    test

--- sungmin-choi/레벨2/test.js
+++ sungmin-choi/레벨2/test.js
@@ -1,2 +1,15 @@
-"use strict";
-delete Object.prototype; // TypeError 발생
+function Accumulator(startingValue) {
+  this.startingValue = startingValue;
+  this.value = startingValue;
+  this.read = function () {
+    const num = prompt("더할 값을 입력해주세요.", 0);
+    this.value += num;
+  };
+}
+
+let accumulator = new Accumulator(1); // 최초값: 1
+
+accumulator.read(); // 사용자가 입력한 값을 더해줌
+accumulator.read(); // 사용자가 입력한 값을 더해줌
+
+alert(accumulator.value); // 최초값과 사용자가 입력한 모든 값을 더해 출력함
