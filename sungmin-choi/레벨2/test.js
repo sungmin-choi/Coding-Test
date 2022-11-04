4023ed89d64eb82fc49524bc1d8eb320c9b369e5
sungmin choi <namja306@naver.com>
2022-11-05 03:03:16 +0900

    ..

--- sungmin-choi/레벨2/test.js
+++ sungmin-choi/레벨2/test.js
@@ -1,9 +1,52 @@
-Function.prototype.defer = function defer(time) {
-  return (a, b) => setTimeout(() => this.call(this, a, b), time);
-};
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
 
-function f(a, b) {
-  console.log(a + b);
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = "0" + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = "0" + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = "0" + secs;
+
+    let output = this.template
+      .replace("h", hours)
+      .replace("m", mins)
+      .replace("s", secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
 }
 
-f.defer(1000)(1, 2);
+class ExtendedClock extends Clock {
+  constructor({ template, precision }) {
+    super({ template });
+    this.precision = precision ? precision : 1000;
+  }
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), this.precision);
+  }
+}
+
+let lowResolutionClock = new ExtendedClock({
+  template: "h:m:s",
+  precision: 2000,
+});
+
+lowResolutionClock.start();
