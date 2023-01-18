30e2556888de3df79357b9a3d78495eda7977d0a
sungmin choi <namja306@naver.com>
2023-01-18 22:06:41 +0900

    delete) main.dart

---
+++ test.js
@@ -0,0 +1,20 @@
+function solution(babbling) {
+  const r1 = /(aya|ye|woo|ma)\1/;
+  const r2 = /^(aya|ye|woo|ma)+$/;
+  let answer = 0;
+  for (const b of babbling) {
+    const m = r1.exec(b);
+
+    console.log("m:", m);
+    if (m) continue;
+
+    const m2 = r2.exec(b);
+    if (!m2) continue;
+
+    answer++;
+  }
+
+  return answer;
+}
+
+solution(["yeayaye", "uuu", "yeye", "yemawoo", "yeayaayaa"]);
