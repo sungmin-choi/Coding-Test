66a615f20113c523cb94299fb98b6e6ef00622d4
sungmin choi <namja306@naver.com>
2023-01-20 19:19:23 +0900

    정규식문제1

---
+++ sungmin-choi/레벨1/신규_아이디_추천.js
@@ -0,0 +1,12 @@
+function solution(new_id) {
+  var answer = "";
+  new_id.toLowerCase();
+
+  return answer;
+}
+
+solution("...!@BaT#*..y.abcdefghijklm");
+
+const regx = /^[0-9][a-b]/g;
+
+console.log("...!@bat#*..y.abcdefghijklm".toLowerCase());
