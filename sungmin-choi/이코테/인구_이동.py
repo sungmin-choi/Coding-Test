7c4334b7f2abe6d5774f2279419d4cf584976ca9
sungmin choi <namja306@naver.com>
2023-03-24 00:08:20 +0900

    solved 인구 이동

--- sungmin-choi/이코테/인구_이동.py
+++ sungmin-choi/이코테/인구_이동.py
@@ -1,79 +1,72 @@
+import sys
 from collections import deque
+def input(): return sys.stdin.readline().strip()
 
 N,L,R =   list(map(int,input().split()))
 
 
 graph = []
 
-for i in range(N):
-    graph.append(list(map(int, input().split())))
+dx=[0,1,-1,0]
+dy=[1,0,0,-1]
 
 
+graph = [list(map(int, input().split())) for _ in range(N)]
 
- 
 
-def dfs(q, cnt, total, trans_arr, visited, graph):
-    dx = [0,-1,0,1]
-    dy = [1,0,-1,0]
-    cnt = cnt
-    total = total
+def dfs(q, visited, cnt):
+    united = []
     while q:
-        x, y = q.popleft()
-        visited[x][y] = True
-        cnt +=1
-        total +=graph[x][y]
-        trans_arr.append((x,y))
+        x,y = q.popleft()
+        united.append((x,y))
+        visited[x][y] = cnt
         for k in range(4):
-            nx,ny = x+dx[k], y+dy[k]
-            if nx>=0 and nx<N and ny>=0 and ny<N:
-                period = abs(graph[x][y] - graph[nx][ny])
-                if (visited[nx][ny] ==False) and  (period >=L and period<=R):
-                    q.append((nx,ny))
-    return cnt, total
-            
-
-
-def solution(N, L,R, graph):
-    dx = [0,-1,0,1]
-    dy = [1,0,-1,0]
-    result = 0
+            nx, ny = x+dx[k], y+dy[k]
+            if 0 <= nx < N and 0<= ny < N and visited[nx][ny] == -1 and L <= abs(graph[x][y] - graph[nx][ny]) <=R:
+                q.append((nx,ny))
     
-    flag = True
+    total = 0
+    count = 0
+    for x,y in united:
+        total +=graph[x][y]
+        count +=1
+    
+    next_num = total//count
 
+    for x,y in united:
+        graph[x][y] = next_num
+
+
+def process():
+    result = 0
+    flag = True
     while flag:
+        visited = [[-1] * N for _ in range(N)]
+        cnt = 1
         flag = False
-        visited = [[False]*N for _ in range(N)]
         for i in range(N):
             for j in range(N):
-                if visited[i][j] == False:
-                    arr = []
+                if visited[i][j] == -1:
                     for k in range(4):
-                        nx,ny = i+dx[k], j+dy[k]
-                        if nx>=0 and nx<N and ny>=0 and ny<N:
-                            period = abs(graph[i][j] - graph[nx][ny])
-                            if  (visited[nx][ny] ==False) and  (period >=L and period<=R):
-                                
+                        nx, ny = i+dx[k], j+dy[k]
+                        if 0 <= nx < N and 0<= ny < N and visited[nx][ny] == -1 and L <= abs(graph[i][j] - graph[nx][ny]) <=R:
+                                q = deque([(i,j)])
+                                dfs(q,visited,cnt)
+                                cnt +=1
                                 flag = True
-                                arr.append((nx,ny))
-                    if len(arr) > 0:
-                        trans_arr = [(i,j)]
-                        total = graph[i][j]
-                        cnt = 1
-                        visited[i][j] = True
-                        q = deque([])
-                        for x, y in arr:
-                            q.append((x,y))
-                        cnt, total =  dfs(q, cnt, total, trans_arr, visited, graph)
-                        a = int(total/cnt)
-                        for cx,cy in trans_arr:
-                            graph[cx][cy] = a
-                        result +=1
-    
+        if flag:
+            result +=1
+        
     print(result)
 
+process()
+                
+
+                        
 
 
-solution(N,L,R,graph)
+    
+
 
                     
 
